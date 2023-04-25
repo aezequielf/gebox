8c2824a4a239758e0006d60a2d822acc0ef58128
Ezequiel <qferro@gmail.com>
2023-04-24 21:11:10 -0300

    creando los primeros modelos

--- agenda/models.py
+++ agenda/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class perfil(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    peso = models.IntegerField(null = True, blank = True)
+
 class turnos(models.Model):
-    pass
+    dia_hora = models.DateTimeField(null = True, blank = True)
+    activo = models.BooleanField(default = True)
+    detalle = models.CharField(max_length = 60, null = True, blank = True)
+    def __str__(self):
+        return f' Turno {self.dia_hora} '
 
 class grupos(models.Model):
-    pass
+    turno = models.ForeignKey(turnos, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+ 
