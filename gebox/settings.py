a569bc081b10e2101b13068ff8da4dae53003828
Ezequiel <eferro@riotel.com.ar>
2023-05-15 10:36:36 -0300

    agregamos STATIC_ROOT = os.path.join(BASE_DIR, 'static/') para que los archivos sean accesibles por nginx para servirlos en producción, Nota: manage.py collectstatic

--- gebox/settings.py
+++ gebox/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     join(BASE_DIR, 'gebox', 'static'),
