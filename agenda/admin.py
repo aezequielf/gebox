3b4a37f147dc3e88f06a79b8952534a7c0794b69
Ezequiel <qferro@gmail.com>
2023-05-16 21:17:14 -0300

    Mdificaciones varias, primera idea de como ver la aenda

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import turnos, grupos
+from .models import turnos, grupos, params_ag
 
 admin.site.register(turnos)
 admin.site.register(grupos)
+admin.site.register(params_ag)
--- agenda/urls.py
+++ agenda/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('' , views.index_agenda, name='index_agenda'),
+    path('diaria/', views.index_diario, name='index_diario'),
+    path('diaria/hora/', views.index_hora, name = 'index_horaria')
 ]
